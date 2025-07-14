39f5ce53c1e30060cfc7d8ff290c5b83c2851d75
out1312 <outualva@gmail.com>
2025-07-14 17:38:43 +0200

    Icon improvements for dwmblocks

--- Documents/Suckless/dwm/config.h
+++ Documents/Suckless/dwm/config.h
@@ -4,7 +4,7 @@ static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMonoNerdFontMono-Medium:size=10" };
+static const char *fonts[]          = { "JetBrainsMonoNerdFont-Medium:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_black[]       = "#000000";
 static const char col_lblack[]       = "#150f0f";
