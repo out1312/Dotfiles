bc49a024b275720e61772bb18609593ec40c9a2e
out1312 <outualva@gmail.com>
2025-07-17 23:20:18 +0200

    Added specific timeout to volume and screenlight notifications

--- Documents/scripts/screenlight.sh
+++ Documents/scripts/screenlight.sh
@@ -6,11 +6,11 @@ case $1 in
   up)
     brightnessctl set 5%+
     brightness=$(brightnessctl -m | awk -F, '{print $4}')
-    dunstify -h int:value:"$brightness" "Brillo: $brightness"
+    dunstify -t 2000 -h int:value:"$brightness" "Brillo: $brightness"
     ;;
   down)
     brightnessctl set 5%-
     brightness=$(brightnessctl -m | awk -F, '{print $4}')
-    dunstify -h int:value:"$brightness" "Brillo: $brightness"
+    dunstify -t 2000 -h int:value:"$brightness" "Brillo: $brightness"
     ;;
 esac
