7594a06764829200a37b4bbf63691ba5bedcf726
out1312 <outualva@gmail.com>
2025-08-03 06:03:14 +0200

    Removed duplicate rust-analyzer in nvim. Tried to sped up starship

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -79,29 +79,30 @@ return {
     name = "catppuccin",
     priority = 1000,
   },
-  {
-    "simrat39/rust-tools.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
-    ft = { "rust" },
-    config = function()
-        require("rust-tools").setup({
-            tools = {
-                inlay_hints = {
-                    auto = false,  -- Disable automatic inlay hints
-                    show_parameter_hints = false,
-                    show_variable_name_hints = false,
-                    only_current_line = false,
-                },
-            },
-            server = {
-                on_attach = function(_, bufnr)
-                    vim.keymap.set("n", "<leader>n", vim.lsp.buf.rename,
-                        { buffer = bufnr, desc = "Rename variable" })
-                end,
-            },
-        })
-    end
-  },
+
+  -- {
+  --   "simrat39/rust-tools.nvim",
+  --   dependencies = { "neovim/nvim-lspconfig" },
+  --   ft = { "rust" },
+  --   config = function()
+  --       require("rust-tools").setup({
+  --           tools = {
+  --               inlay_hints = {
+  --                   auto = false,  -- Disable automatic inlay hints
+  --                   show_parameter_hints = false,
+  --                   show_variable_name_hints = false,
+  --                   only_current_line = false,
+  --               },
+  --           },
+  --           server = {
+  --               on_attach = function(_, bufnr)
+  --                   vim.keymap.set("n", "<leader>n", vim.lsp.buf.rename,
+  --                       { buffer = bufnr, desc = "Rename variable" })
+  --               end,
+  --           },
+  --       })
+  --   end
+  -- },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
