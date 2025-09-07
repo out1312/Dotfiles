27e6bd3811ff67817fb31ad1542962fa4c30f282
out1312 <outualva@gmail.com>
2025-09-07 05:10:48 +0200

    Added slock to dwm

--- Documents/Suckless/dwm/config.h
+++ Documents/Suckless/dwm/config.h
@@ -73,6 +73,7 @@ static const Key keys[] = {
 	{ Mod4Mask,                     XK_d,      spawn,          SHCMD("dolphin") },
 	{ Mod4Mask,                     XK_q,      spawn,          SHCMD("dmenu_kill.sh") },
 	{ Mod4Mask,                     XK_c,      spawn,          SHCMD("dmenu_cine.sh") },
+	{ Mod4Mask,                     XK_l,      spawn,          SHCMD("slock") },
 	{ Mod4Mask|ShiftMask,           XK_s,      spawn,          SHCMD("flameshot gui") },
 	{ MODKEY,                       XK_F6,     spawn,          SHCMD("screenlight.sh up") },
 	{ MODKEY,                       XK_F5,     spawn,          SHCMD("screenlight.sh down") },
